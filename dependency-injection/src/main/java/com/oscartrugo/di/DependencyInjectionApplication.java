bbb270d59a0e1bc37172aa8b6558d20a548e0c8b
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-09-09 23:45:26 -0600

    Eliminando imports innecesarios

--- dependency-injection/src/main/java/com/oscartrugo/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/oscartrugo/di/DependencyInjectionApplication.java
@@ -3,14 +3,10 @@ package com.oscartrugo.di;
 import com.oscartrugo.di.autowire.AreaCalculatorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
