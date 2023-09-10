af6e7e1869d06710b347248a7bca153f7f8c8bd2
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-09-09 19:35:15 -0600

    18. Definición de Beans de forma explícita.
    Definición de Beans de forma explícita mediante la anotación @Bean.

--- dependency-injection/src/main/java/com/oscartrugo/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/oscartrugo/di/DependencyInjectionApplication.java
@@ -1,47 +1,27 @@
 package com.oscartrugo.di;
 
-import com.oscartrugo.di.atributo.Coche;
-import com.oscartrugo.di.profiles.EnvironmentService;
-import com.oscartrugo.di.qualifiers.*;
-import com.oscartrugo.di.scopes.EjemploScopeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(DependencyInjectionApplication.class);
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
-		EjemploScopeService ejemploScopeService = context.getBean(EjemploScopeService.class);
-		EjemploScopeService ejemploScopeService1 = context.getBean(EjemploScopeService.class);
-
-		log.info("Are beans equal {}", ejemploScopeService.equals(ejemploScopeService1));
-		log.info("Are beans == {} ", (ejemploScopeService == ejemploScopeService1) );
-
-
-
-		/*		Coche coche = context.getBean(Coche.class); //Obtenemos los beans de la clase Coche
-		Perro perro = context.getBean(Perro.class);
-		Pajaro pajaro = context.getBean(Pajaro.class);
-		Avion avion = context.getBean(Avion.class);
-		Animal animal = context.getBean("pajaro",Animal.class);
-		Animal animal2 = context.getBean("perro",Animal.class);
-
-		log.info("Objeto Coche {}", coche.getModelo());
-		log.info("Objeto Perro {}", perro.getNombre());
-		log.info("Objeto Pájaro {}", pajaro.getNombre());
-		log.info("Objeto Avon {}", avion.toString());
-		log.info("Objeto Animal - Pájaro {}", animal.getNombre());
-		log.info("Objeto Animal - Perro {}", animal2.getNombre());
-
-		Nido animalNido = context.getBean(Nido.class);
-		animalNido.imprimir();*/
-		//context.getBean("qualifier", Animal.class);
+	@Bean 
+	public String getApplicationName(){
+		return "DependencyInjectionApplication";
 	}
 
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
+		String nombreAplicacion = context.getBean(String.class);
+
+		log.info("Nombre de la aplicación: " + nombreAplicacion);
+	}
 }
