7cb15feb7b1bc033757a93f6cb8c10775270a0a1
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-09-09 22:46:39 -0600

    20. Uso de anotación @SpringBootApplication
    La anotación @SpringBootApplication no es más que una simplificación de las anotaciones @Configuration, @ComponentScan y @EnableAutoConfiguration. En vez de usar las 3, usamos la primera.

--- dependency-injection/src/main/java/com/oscartrugo/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/oscartrugo/di/DependencyInjectionApplication.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
