16fe27d25cd03c1f8e416f09b5c66fa2943e643f
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-09-13 21:29:09 -0600

    35. Accediendo a la información del "Joint Point"

--- dependency-injection/src/main/java/com/oscartrugo/di/aop/MyAspect.java
+++ dependency-injection/src/main/java/com/oscartrugo/di/aop/MyAspect.java
@@ -1,11 +1,14 @@
 package com.oscartrugo.di.aop;
 
 import com.oscartrugo.di.DependencyInjectionApplication;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Modifier;
 
 @Component
 @Aspect
@@ -15,7 +18,11 @@ public class MyAspect {
 
     //Si en vez de hello ponemos *, aplica el before advice para todos los métodos
     @Before("execution(* com.oscartrugo.di.aop.TargetObject.hello(..))")
-    public void before(){
+    public void before(JoinPoint jointPoint){
+        log.info("Method name {}", jointPoint.getSignature().getName());
+        log.info("Object type {}", jointPoint.getSignature().getDeclaringTypeName());
+        log.info("Is public {}", Modifier.isPublic(jointPoint.getSignature().getModifiers()));
+        log.info("Arguments {}", jointPoint.getArgs());
         log.info("Before advice");
     }
 
