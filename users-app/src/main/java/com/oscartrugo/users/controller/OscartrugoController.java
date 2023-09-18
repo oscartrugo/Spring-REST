98d564790cc4472c17d18e2417637337d1e99aa2
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-09-17 19:48:33 -0600

    51. Definición de Controllers

---
+++ users-app/src/main/java/com/oscartrugo/users/controller/OscartrugoController.java
@@ -0,0 +1,18 @@
+package com.oscartrugo.users.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Stereotype que registra la aplicación en el contexto de Spring
+@RequestMapping("/hello") //Define el recurso "hello"
+@Slf4j
+public class OscartrugoController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
+}
