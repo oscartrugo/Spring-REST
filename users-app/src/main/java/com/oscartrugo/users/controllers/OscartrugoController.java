209c692a9b7ae1c86082572f34391028c864900a
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-09-17 20:03:25 -0600

    52. Configuración de Java Faker

--- users-app/src/main/java/com/oscartrugo/users/controllers/OscartrugoController.java
+++ users-app/src/main/java/com/oscartrugo/users/controllers/OscartrugoController.java
@@ -1,4 +1,4 @@
-package com.oscartrugo.users.controller;
+package com.oscartrugo.users.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ users-app/src/main/java/com/oscartrugo/users/services/UserService.java
@@ -0,0 +1,16 @@
+package com.oscartrugo.users.services;
+
+import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private Faker faker;
+    /*
+    * Código para mi servicio
+    * */
+
+}
