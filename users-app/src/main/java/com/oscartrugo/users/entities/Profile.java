f1ef91ab14b6a8f6117e949dbbcc5e549b7eae06
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-10-14 00:14:43 -0600

    65. Creando entidades Address y Role @ManyToOne

--- users-app/src/main/java/com/oscartrugo/users/entities/Profile.java
+++ users-app/src/main/java/com/oscartrugo/users/entities/Profile.java
@@ -3,6 +3,7 @@ package com.oscartrugo.users.entities;
 import jakarta.persistence.*;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "profile")
@@ -20,4 +21,49 @@ public class Profile {
 
     @Column(name = "birt_date")
     private Date birthDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(Integer firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(Integer lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(id, profile.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- users-app/src/main/java/com/oscartrugo/users/entities/Role.java
+++ users-app/src/main/java/com/oscartrugo/users/entities/Role.java
@@ -1,4 +1,46 @@
 package com.oscartrugo.users.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "role")
 public class Role {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
