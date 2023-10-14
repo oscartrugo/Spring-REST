baddf1dc433112aff262662a685498209034ac87
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-10-14 00:19:51 -0600

    66. Agregando la entidad UserInRole

--- users-app/src/main/java/com/oscartrugo/users/entities/UserInRole.java
+++ users-app/src/main/java/com/oscartrugo/users/entities/UserInRole.java
@@ -1,4 +1,58 @@
 package com.oscartrugo.users.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "user_in_role")
 public class UserInRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInRole that = (UserInRole) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
